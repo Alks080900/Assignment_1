8774968b8b7f45fadbd9b65397ec58d7808484f7
AlksOblg <alexanderoblego4570089@gmail.com>
2024-11-27 11:27:46 -0600

    Alex branch

--- problem1and2.java
+++ problem1and2.java
@@ -2,6 +2,7 @@ package Assignment_1;
 
 --sample new text here--
     --added text here on github--
+--Added text inside alex branch--
 public class problem1and2 {
     public static void main(String[] args) {
         String emojieWavingHand = "\uD83D\uDC4B";
